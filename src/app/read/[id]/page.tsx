79ea893fcb703137e28e6926650671650803ecf4
anibal andrade <59257555+androso@users.noreply.github.com>
2024-12-15 22:01:02 +0000

    adding reader with dynamic book id

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -6,7 +6,7 @@ import { useParams } from "next/navigation";
 
 export default function Reader() {
 	const params = useParams();
-	const bookId = params.bookId as string | null;
+	const bookId = params.id as string | null;
 	return (
 		<div className="h-screen w-screen flex flex-col">
 			<BookViewer bookId={bookId ?? ""} />
