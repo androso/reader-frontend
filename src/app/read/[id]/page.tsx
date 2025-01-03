497b36c9a2f9098a0cf612d18ed9a410cdda43af
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-03 23:26:06 +0000

    adapting content to screen

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -11,12 +11,12 @@ export default function Reader() {
 	const bookId = params.id as string | null;
 
 	return (
-		<div className="min-h-screen bg-[#D7D7D7] p-8">
-			<div className="w-full rounded-lg relative flex justify-center">
+		<div className="max-h-screen bg-[#D7D7D7] ">
+			<div className="max-h-screen w-full rounded-lg relative flex justify-center p-8">
 				<div className="w-[48%] bg-[#FCFCFC] mr-4 rounded-lg">
 					<ChatInterface />
 				</div>
-				<div className="h-[789px] w-[48%] relative overflow-hidden bg-[#FCFCFC] rounded-lg">
+				<div className=" w-[48%] relative overflow-hidden bg-[#FCFCFC] rounded-lg">
 					<EpubReader url={`/api/books/${bookId}`} />
 				</div>
 			</div>
