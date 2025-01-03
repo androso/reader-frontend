1842532931e6e7b5a39efcfe9901ce28fb3b4add
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-03 23:03:12 +0000

    adding chat interface and improving styles

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -4,15 +4,19 @@
 import { useParams } from "next/navigation";
 import EpubReader from "@/components/reader-test/EpubReader";
 import { useWindowSize } from "@/hooks/useWindowSize";
+import { ChatInterface } from "@/components/reader/ChatInterface";
 
 export default function Reader() {
 	const params = useParams();
 	const bookId = params.id as string | null;
 
 	return (
-		<div className="min-h-screen bg-red-200 flex items-center justify-center p-4">
-			<div className="w-full max-w-4xl bg-white rounded-lg shadow-lg relative">
-				<div className="h-[789px] relative overflow-hidden">
+		<div className="min-h-screen bg-[#D7D7D7] p-8">
+			<div className="w-full rounded-lg relative flex justify-center">
+				<div className="w-[48%] bg-[#FCFCFC] mr-4 rounded-lg">
+					<ChatInterface />
+				</div>
+				<div className="h-[789px] w-[48%] relative overflow-hidden bg-[#FCFCFC] rounded-lg">
 					<EpubReader url={`/api/books/${bookId}`} />
 				</div>
 			</div>
