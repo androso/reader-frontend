bdaa4c38a0726fc202d7aec39a96ca60c5e860b2
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-03 23:34:04 +0000

    hiding chat interface on mobile

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -9,14 +9,18 @@ import { ChatInterface } from "@/components/reader/ChatInterface";
 export default function Reader() {
 	const params = useParams();
 	const bookId = params.id as string | null;
-
+	const { width } = useWindowSize();
+	const isMobile = width < 768;
+	
 	return (
 		<div className="h-screen bg-[#D7D7D7] ">
 			<div className="h-screen w-full rounded-lg relative flex justify-center p-8">
-				<div className="w-[48%] bg-[#FCFCFC] mr-4 rounded-lg">
-					<ChatInterface />
-				</div>
-				<div className=" w-[48%] relative overflow-hidden bg-[#FCFCFC] rounded-lg">
+				{!isMobile && (
+					<div className="w-[48%] bg-[#FCFCFC] mr-4 rounded-lg">
+						<ChatInterface />
+					</div>
+				)}
+				<div className={`w-${isMobile ? '[100%]' : '[48%]'} relative overflow-hidden bg-[#FCFCFC] rounded-lg`}>
 					<EpubReader url={`/api/books/${bookId}`} />
 				</div>
 			</div>
