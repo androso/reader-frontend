ba24be0472628fd350e5861c195ce47baf306110
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-15 01:37:11 +0000

    fresh login state added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Card } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { useGoogleSignIn, useUser } from "@/lib/auth";
 import { useGoogleLogin } from "@react-oauth/google";
 import { GoogleSignInButton } from "@/components/GoogleSignInButton";
@@ -10,20 +9,17 @@ import { LoadingSpinner } from "@/components/ui/LoadingSpinner";
 export default function Login() {
 	const router = useRouter();
 	const { data: userData, status: userStatus } = useUser();
-	const {
-		mutateAsync: signIn,
-		isPending: googlePending,
-	} = useGoogleSignIn();
+	const { mutateAsync: signIn, isPending: googlePending, status: googleStatus } = useGoogleSignIn();
 
-	useEffect(() => {
-		if (userData) {
-			router.push("/");
-		}
-	}, [userData, router]);
-
+	// useEffect(() => {
+	// 	if (userData) {
+	// 		router.push("/");
+	// 	}
+	// }, [userData, router]);
 	const login = useGoogleLogin({
 		onSuccess: async (codeResponse) => {
 			await signIn(codeResponse.access_token);
+			router.push("/")
 		},
 		onError: (error) => {
 			console.error("Login Failed:", error);
@@ -31,15 +27,22 @@ export default function Login() {
 	});
 
 	// Show loading state while redirecting
-	if (userStatus == "pending" && googlePending) {
+	if (userStatus == "pending" && (googlePending || googleStatus == "success")) {
 		return <LoadingSpinner />;
 	}
-
+	
 	return (
 		<div className="container mx-auto flex items-center justify-center min-h-screen">
 			<Card className="w-full max-w-md p-6">
-				<h1 className="text-2xl font-semibold text-center mb-6">Login</h1>
-				<GoogleSignInButton onClick={() => login()} isLoading={googlePending} />
+				<h1 className="text-2xl font-semibold text-center mb-6">
+					Login
+				</h1>
+				<GoogleSignInButton
+					onClick={() => {
+						login();
+					}}
+					isLoading={googlePending}
+				/>
 			</Card>
 		</div>
 	);
