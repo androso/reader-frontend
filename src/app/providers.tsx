3a4d0be47e5f0cc5a71b4549de5d4e32254519cc
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-15 03:33:36 +0000

    using react-hot-toast

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,17 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<GoogleOAuthProvider
+			clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+		>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+			<Toaster />
 		</GoogleOAuthProvider>
 	);
 }
