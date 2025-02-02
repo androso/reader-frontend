117519dc598a8199cde6a5d2770806a53ef7202f
Zack-y11 <dev.zacknmr28@gmail.com>
2025-02-02 16:11:13 -0600

    refactor: simplify component structure and update import paths for ReactReader

---
+++ src/components/reader/EpubView.tsx
@@ -0,0 +1,184 @@
+import React, { Component } from "react";
+import Epub, { Book } from "epubjs";
+import type { NavItem, Contents, Rendition, Location } from "epubjs";
+import { EpubViewStyle as defaultStyles, type IEpubViewStyle } from "./style";
+import type { RenditionOptions } from "epubjs/types/rendition";
+import type { BookOptions } from "epubjs/types/book";
+
+export type RenditionOptionsFix = RenditionOptions & {
+    allowPopups: boolean;
+};
+
+export type IToc = {
+    label: string;
+    href: string;
+};
+
+export type IEpubViewProps = {
+    url: string | ArrayBuffer;
+    epubInitOptions?: Partial<BookOptions>;
+    epubOptions?: Partial<RenditionOptionsFix>;
+    epubViewStyles?: IEpubViewStyle;
+    loadingView?: React.ReactNode;
+    location: string | number | null;
+    locationChanged(value: string): void;
+    showToc?: boolean;
+    tocChanged?(value: NavItem[]): void;
+    getRendition?(rendition: Rendition): void;
+    handleKeyPress?(): void;
+    handleTextSelected?(cfiRange: string, contents: Contents): void;
+};
+type IEpubViewState = {
+    isLoaded: boolean;
+    toc: NavItem[];
+};
+
+export class EpubView extends Component<IEpubViewProps, IEpubViewState> {
+    state: Readonly<IEpubViewState> = {
+        isLoaded: false,
+        toc: [],
+    };
+    viewerRef = React.createRef<HTMLDivElement>();
+    location?: string | number | null;
+    book?: Book;
+    rendition?: Rendition;
+    prevPage?: () => void;
+    nextPage?: () => void;
+
+    constructor(props: IEpubViewProps) {
+        super(props);
+        this.location = props.location;
+        this.book = this.rendition = this.prevPage = this.nextPage = undefined;
+    }
+
+    componentDidMount() {
+        this.initBook();
+        document.addEventListener("keyup", this.handleKeyPress, false);
+    }
+
+    initBook() {
+        const { url, tocChanged, epubInitOptions } = this.props;
+        if (this.book) {
+            this.book.destroy();
+        }
+        this.book = Epub(url);
+        this.book.loaded.navigation.then(({ toc }) => {
+            this.setState(
+                {
+                    isLoaded: true,
+                    toc: toc,
+                },
+                () => {
+                    tocChanged && tocChanged(toc);
+                    this.initReader();
+                }
+            );
+        });
+    }
+
+    componentWillUnmount() {
+        if (this.book) {
+            this.book.destroy();
+        }
+        this.book = this.rendition = this.prevPage = this.nextPage = undefined;
+        document.removeEventListener("keyup", this.handleKeyPress, false);
+    }
+
+    shouldComponentUpdate(nextProps: IEpubViewProps) {
+        return (
+            !this.state.isLoaded ||
+            nextProps.location !== this.props.location ||
+            nextProps.url !== this.props.url
+        );
+    }
+
+    componentDidUpdate(prevProps: IEpubViewProps) {
+        if (
+            prevProps.location !== this.props.location &&
+            this.location !== this.props.location
+        ) {
+            this.rendition?.display(this.props.location + "");
+        }
+        if (prevProps.url !== this.props.url) {
+            this.initBook();
+        }
+    }
+
+    initReader() {
+        const { toc } = this.state;
+        const { location, epubOptions, getRendition } = this.props;
+        if (this.viewerRef.current) {
+            const node = this.viewerRef.current;
+            if (this.book) {
+                const rendition = this.book.renderTo(node, {
+                    manager: "continuous",
+                    flow: "scrolled",
+                    width: "100%",
+                    height: "100%",
+                });
+                rendition.themes.register("default", {
+                    "*, *::before, *::after": {
+                        "font-size": "18px",
+                        "line-height": "1.6",
+                        "-webkit-font-smoothing": "antialiased",
+                    },
+                    p: {
+                        margin: "1em 0",
+                    },
+                });
+                rendition.themes.select("default");
+                this.rendition = rendition;
+                getRendition && getRendition(rendition);
+
+                // Skip cover by starting from second TOC item or falling back to spine
+                if (
+                    typeof location === "string" ||
+                    typeof location === "number"
+                ) {
+                    rendition.display(location + "");
+                } else if (toc.length > 0 && toc[0].href) {
+                    rendition.display(toc[0].href);
+                } else {
+                    rendition.display();
+                }
+            }
+        }
+    }
+
+    registerEvents() {}
+
+    onLocationChange = (loc: Location) => {
+        const { location, locationChanged } = this.props;
+        const newLocation = `${loc.start}`;
+        if (location !== newLocation) {
+            this.location = newLocation;
+            locationChanged && locationChanged(newLocation);
+        }
+    };
+
+    renderBook() {
+        const epubViewStyles = {
+            ...defaultStyles,
+            ...this.props.epubViewStyles,
+        };
+        return (
+            <div ref={this.viewerRef} style={epubViewStyles.view} id="huh" />
+        );
+    }
+
+    handleKeyPress = (event: KeyboardEvent) => {};
+
+    render() {
+        const { isLoaded } = this.state;
+        const { loadingView = null } = this.props;
+        const epubViewStyles = {
+            ...defaultStyles,
+            ...this.props.epubViewStyles,
+        };
+        return (
+            <div style={epubViewStyles.viewHolder}>
+                {(isLoaded && this.renderBook()) || loadingView}
+            </div>
+        );
+    }
+}
---
+++ src/components/reader/ReactReader.tsx
@@ -0,0 +1,190 @@
+import React, {
+    type CSSProperties,
+    type ReactNode,
+    useRef,
+    useState,
+} from "react";
+import { type SwipeableProps, useSwipeable } from "react-swipeable";
+import { EpubView, type IEpubViewProps } from "./EpubView/EpubView";
+import { type IEpubViewStyle } from "./EpubView/style";
+import {
+    ReactReaderStyle as defaultStyles,
+    type IReactReaderStyle,
+} from "./style";
+
+import { type NavItem } from "epubjs";
+
+type SwipeWrapperProps = {
+    children: ReactNode;
+    swipeProps: Partial<SwipeableProps>;
+};
+
+const SwipeWrapper = ({ children, swipeProps }: SwipeWrapperProps) => {
+    const handlers = useSwipeable(swipeProps);
+    return (
+        <div style={{ height: "100%" }} {...handlers}>
+            {children}
+        </div>
+    );
+};
+
+type TocItemProps = {
+    data: NavItem;
+    setLocation: (value: string) => void;
+    styles?: CSSProperties;
+};
+
+const TocItem = ({ data, setLocation, styles }: TocItemProps) => (
+    <div>
+        <button onClick={() => setLocation(data.href)} style={styles}>
+            {data.label}
+        </button>
+        {data.subitems && data.subitems.length > 0 && (
+            <div style={{ paddingLeft: 10 }}>
+                {data.subitems.map((item, i) => (
+                    <TocItem
+                        key={i}
+                        data={item}
+                        styles={styles}
+                        setLocation={setLocation}
+                    />
+                ))}
+            </div>
+        )}
+    </div>
+);
+
+export type IReactReaderProps = IEpubViewProps & {
+    title?: string;
+    showToc?: boolean;
+    readerStyles?: IReactReaderStyle;
+    epubViewStyles?: IEpubViewStyle;
+    swipeable?: boolean;
+    isRTL?: boolean;
+};
+
+export const ReactReader = ({
+    title,
+    showToc = true,
+    loadingView,
+    readerStyles = defaultStyles,
+    locationChanged,
+    swipeable,
+    epubViewStyles,
+    isRTL = false,
+    tocChanged,
+    ...props
+}: IReactReaderProps) => {
+    const [expandedToc, setExpandedToc] = useState(false);
+    const [toc, setToc] = useState<NavItem[]>([]);
+    const readerRef = useRef<EpubView>(null);
+    const toggleToc = () => {
+        setExpandedToc(!expandedToc);
+    };
+
+    const handleTocChange = (newToc: NavItem[]) => {
+        // Filter out the first item (cover)
+        const filteredToc = newToc.slice(1);
+        setToc(filteredToc);
+        if (tocChanged) {
+            tocChanged(filteredToc);
+        }
+    };
+
+    const handleSetLocation = (loc: string) => {
+        setExpandedToc(false);
+        if (locationChanged) {
+            locationChanged(loc);
+        }
+    };
+
+    return (
+        <div style={readerStyles.container} className="rounded-lg">
+            <div
+                style={Object.assign(
+                    {},
+                    readerStyles.readerArea,
+                    expandedToc ? readerStyles.containerExpanded : {}
+                )}
+            >
+                {showToc && (
+                    <button
+                        style={Object.assign(
+                            {},
+                            readerStyles.tocButton,
+                            expandedToc ? readerStyles.tocButtonExpanded : {}
+                        )}
+                        onClick={toggleToc}
+                    >
+                        <span
+                            style={Object.assign(
+                                {},
+                                readerStyles.tocButtonBar,
+                                readerStyles.tocButtonBarTop
+                            )}
+                        />
+                        <span
+                            style={Object.assign(
+                                {},
+                                readerStyles.tocButtonBar,
+                                readerStyles.tocButtonBottom
+                            )}
+                        />
+                    </button>
+                )}
+                <div style={readerStyles.titleArea}>{title}</div>
+                <SwipeWrapper
+                    swipeProps={{
+                        onTouchStartOrOnMouseDown: ({ event }) =>
+                            event.preventDefault(),
+                        touchEventOptions: { passive: false },
+                        preventScrollOnSwipe: true,
+                        trackMouse: true,
+                    }}
+                >
+                    <div style={readerStyles.reader}>
+                        <EpubView
+                            ref={readerRef}
+                            loadingView={
+                                loadingView === undefined ? (
+                                    <div style={readerStyles.loadingView}>
+                                        Loading…
+                                    </div>
+                                ) : (
+                                    loadingView
+                                )
+                            }
+                            epubViewStyles={epubViewStyles}
+                            {...props}
+                            tocChanged={handleTocChange}
+                            locationChanged={locationChanged}
+                        />
+                        {swipeable && <div style={readerStyles.swipeWrapper} />}
+                    </div>
+                </SwipeWrapper>
+            </div>
+            {showToc && (
+                <div>
+                    <div style={readerStyles.tocArea}>
+                        <div style={readerStyles.toc}>
+                            {toc.map((item, i) => (
+                                <TocItem
+                                    data={item}
+                                    key={i}
+                                    setLocation={handleSetLocation}
+                                    styles={readerStyles.tocAreaButton}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                    {expandedToc && (
+                        <div
+                            style={readerStyles.tocBackground}
+                            onClick={toggleToc}
+                        />
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
